a9c6e93f26cc749db623960043f2351224852528
ankit0511 <99800154+ankit0511@users.noreply.github.com>
2025-01-08 18:43:15 +0530

    Setting Up FrontEnd

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <> 
+    <div className='text-2xl'>Hi From FrontEnd </div>
+    </>
+  )
+}
+
+export default App
--- index.js
+++ index.js
@@ -1,18 +1,60 @@
 import http from 'http'
 import express from "express"
 import path from 'path'
+import { spawn } from 'child_process';
 import { Server as SocketIO } from 'socket.io';
 
 
 const app = express();
 const server = http.createServer(app);
 const io = new SocketIO(server)
+
+const options = [
+    '-i',
+    '-',
+    '-c:v', 'libx264',
+    '-preset', 'ultrafast',
+    '-tune', 'zerolatency',
+    '-r', `${25}`,
+    '-g', `${25 * 2}`,
+    '-keyint_min', 25,
+    '-crf', '25',
+    '-pix_fmt', 'yuv420p',
+    '-sc_threshold', '0',
+    '-profile:v', 'main',
+    '-level', '3.1',
+    '-c:a', 'aac',
+    '-b:a', '128k',
+    '-ar', 128000 / 4,
+    '-f', 'flv',
+    `rtmp://a.rtmp.youtube.com/live2/`,
+];
+
+const ffmpegProcess = spawn('ffmpeg',options )
+// adding event listners
+ ffmpegProcess.stdout.on("data",(data)=>{
+    console.log(data);
+ });
+
+ ffmpegProcess.stderr.on("data",(data)=>{
+    console.log(data);
+ });
+ ffmpegProcess.stdout.on("close",(data)=>{
+    console.log(data);
+ });
+
+
+
+
 io.on("connection", (socket)=>{
 console.log("socket Connected ", socket.id);
 socket.on('binarystream' ,(stream)=>{
     // this is the data which is coming from the sockets
-    console.log("binary stream is coming ", stream.data);
-    
+    console.log("binary stream is coming ");
+    ffmpegProcess.stdin.write(stream,(error)=>{
+   console.log(error);
+})
+
 })
 })
 
