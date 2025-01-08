5e638fe038afeed03855f9779ef6803314aac145
ankit0511 <99800154+ankit0511@users.noreply.github.com>
2025-01-08 17:26:10 +0530

    Socket implementation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import http from 'http'
 import express from "express"
 import path from 'path'
+import { Server as SocketIO } from 'socket.io';
 
 const app = express();
 const server = http.createServer(app);
